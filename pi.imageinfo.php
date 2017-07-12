77b0db410cca669d7af3f1be6a8d68961c482369
Doug Black <dblack@americanbible.org>
2017-07-12 07:51:52 -0400

    Added error handling

--- pi.imageinfo.php
+++ pi.imageinfo.php
@@ -9,8 +9,26 @@ class Imageinfo {
 
         $url = ee()->TMPL->fetch_param('url');
     
+        if(is_null($url) || empty($url))
+        {
+            return FALSE;
+        }
+
         $parsed = parse_url($url);
+
+        $test = get_headers($url);
+
+        if(!strpos($test[0],"200"))
+        {
+            return FALSE;
+        }
+
         $file = $_SERVER['DOCUMENT_ROOT'];
+
+        if(!file_exists($file.$parsed['path']))
+        {
+            return FALSE;
+        }
 
         $imageinfo = getimagesize($file.$parsed['path']);
 
